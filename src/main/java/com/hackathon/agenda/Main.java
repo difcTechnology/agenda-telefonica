58212c4b41503e7a9458103e61e09f1fbc3960da
Laura <laura.barona@uao.edu.co>
2026-07-21 14:17:37 -0500

    agregado action listener para abotón añadir y modificar

--- src/main/java/com/hackathon/agenda/Main.java
+++ src/main/java/com/hackathon/agenda/Main.java
@@ -1,5 +1,7 @@
 package com.hackathon.agenda;
 
+import com.hackathon.agenda.controlador.AgendaControlador;
+import com.hackathon.agenda.modelo.Agenda;
 import com.hackathon.agenda.vista.VentanaPrincipal;
 
 import javax.swing.*;
@@ -8,7 +10,10 @@ public class Main {
     public static void main(String[] args) {
 
         SwingUtilities.invokeLater(() -> {
+            Agenda agenda = new Agenda();
             VentanaPrincipal ventana = new VentanaPrincipal();
+            AgendaControlador controlador = new AgendaControlador(agenda, ventana);
+            controlador.iniciar();
             ventana.setVisible(true);
         });
     }
--- src/main/java/com/hackathon/agenda/controlador/AgendaControlador.java
+++ src/main/java/com/hackathon/agenda/controlador/AgendaControlador.java
@@ -1,4 +1,28 @@
 package com.hackathon.agenda.controlador;
 
+import com.hackathon.agenda.modelo.Agenda;
+import com.hackathon.agenda.vista.VentanaPrincipal;
+
 public class AgendaControlador {
+
+    private final Agenda agenda;
+    private final VentanaPrincipal vista;
+
+    public AgendaControlador(Agenda agenda, VentanaPrincipal vista) {
+        this.agenda = agenda;
+        this.vista = vista;
+    }
+
+    public void iniciar() {
+        vista.getBtnAnadir().addActionListener(e -> accionAnadir());
+        vista.getBtnModificar().addActionListener(e -> accionModificar());
+    }
+
+    private void accionAnadir() {
+        System.out.println("Se hizo clic en Añadir");
+    }
+
+    private void accionModificar() {
+        System.out.println("Se hizo clic en Modificar");
+    }
 }
